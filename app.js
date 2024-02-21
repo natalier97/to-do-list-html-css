0ab613e9c3da08b8f7dd04a92c94fe3750bddad5
Natalie <rivero_natalie@outlook.com>
2024-02-21 18:34:33 -0500

    working on using enter as an eventlistener

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 
 
 document.querySelector(".submit_button").addEventListener("click", makeList);
+// document.querySelector(".adding_item").addEventListener("keydown", function(event){
+//     if (event.key === "Enter"){
+//         makeList();
+//     }
+// }); --- event.prevent default
+
 
 function makeList() {
-
 const item = document.querySelector(".list");
 const li = document.createElement("li");
+
 li.addEventListener("click", 
 function (){
   li.classList.toggle('completed')  
